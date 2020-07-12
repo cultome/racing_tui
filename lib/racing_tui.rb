c06f8ab5705a5a455ec17c125e71da5fbd477fdf
Carlos Soria <csoria@cultome.io>
2020-07-11 22:19:38 -0500

    Initial commit

---
+++ lib/racing_tui.rb
@@ -0,0 +1,6 @@
+require "racing_tui/version"
+
+module RacingTui
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/racing_tui_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RacingTui do
+  it "has a version number" do
+    expect(RacingTui::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
